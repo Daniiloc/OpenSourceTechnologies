2fd792db83acf8327351bfe1c23995d225db247d
Daniiloc <102352439+Daniiloc@users.noreply.github.com>
2023-12-06 00:35:38 +0500

    add login, registration, authentication (#5)

--- main.py
+++ main.py
@@ -1,44 +1,34 @@
-import smtplib
-from email.mime.text import MIMEText
 from flask_bootstrap import Bootstrap
 
-import api
+import send_email
+from api import api
 from models import *
-from flask_caching import Cache, CachedResponse
+from auth import auth
+from flask_caching import CachedResponse
 from flask import Flask, render_template, request, session, redirect, make_response, flash
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY')
+app.register_blueprint(api)
+app.register_blueprint(auth)
+app.secret_key = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f"postgresql://{os.environ.get('POSTGRES_USER')}"
-    f":{os.environ.get('POSTGRES_PASSWORD')}"
-    f"@{os.environ.get('POSTGRES_HOST')}"
-    f":{os.environ.get('POSTGRES_PORT')}"
-    f"/{os.environ.get('POSTGRES_DB')}"
+    f"postgresql://{postgres_config['POSTGRES_USER']}"
+    f":{postgres_config['POSTGRES_PASSWORD']}"
+    f"@{postgres_config['POSTGRES_HOST']}"
+    f":{postgres_config['POSTGRES_PORT']}"
+    f"/{postgres_config['POSTGRES_DB']}"
 )
-cache = Cache(
-    app,
-    config={
-        "CACHE_TYPE": redis_config["CACHE_TYPE"],
-        "CACHE_KEY_PREFIX": redis_config["CACHE_KEY_PREFIX"],
-        "CACHE_REDIS_HOST": redis_config["CACHE_REDIS_HOST"],
-        "CACHE_REDIS_PORT": redis_config["CACHE_REDIS_PORT"],
-        "CACHE_REDIS_URL": redis_config["CACHE_REDIS_URL"]
-    }
-)
+cache.init_app(app)
+csrf.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
 db.init_app(app)
 Bootstrap(app)
 
 
-@app.route('/clear_cache')
-def clear_cache():
-    cache.clear()
-    return 'Cache cleared'
-
-
 @app.route('/')
 def hello():
-    session.clear()
+    clear_session()
     page = int(request.args.get('page') if request.args.get('page') else 1)
     search = request.args.get('search_string', '')
     if search != '':
@@ -80,9 +70,8 @@ def pokemon_page(pokemon_name):
     pokemon = cache.get(pokemon_name)
     if pokemon:
         return CachedResponse(
-            response=make_response(render_template('pokemon_page.html', pokemon=pokemon),
-                                   timeout=50,
-                                   )
+            response=make_response(render_template('pokemon_page.html', pokemon=pokemon)),
+            timeout=50
         )
     cache.set(pokemon_name, get_pokemon_data(pokemon_name))
     return render_template('pokemon_page.html', pokemon=get_pokemon_data(pokemon_name))
@@ -101,7 +90,7 @@ def pokemon_page_save(pokemon_name):
 @app.route('/<main_pokemon_name>_battle', methods=['GET', 'POST'])
 def battle(main_pokemon_name):
     if request.method == 'GET':
-        session.clear()
+        clear_session()
         session['main_pokemon'] = get_pokemon_data(main_pokemon_name)
 
         if 'opponent_pokemon' not in session:
@@ -117,11 +106,7 @@ def battle(main_pokemon_name):
     else:
         if 'auto_fight' in request.form:
             history = auto_fight_history()
-            fight_row = Battles(main_pokemon=session['main_pokemon']['name'],
-                                opponent_pokemon=session['opponent_pokemon']['name'],
-                                win=winner()['name'])
-            db.session.add(fight_row)
-            db.session.commit()
+            add_row_to_battles()
             return render_template('battle.html',
                                    main_pokemon=get_pokemon_data(session['main_pokemon']['name']),
                                    opponent_pokemon=get_pokemon_data(session['opponent_pokemon']['name']),
@@ -136,11 +121,7 @@ def battle(main_pokemon_name):
                 players_number = None
 
             if define_global_win():
-                fight_row = Battles(main_pokemon=session['main_pokemon']['name'],
-                                    opponent_pokemon=session['opponent_pokemon']['name'],
-                                    win=winner()['name'])
-                db.session.add(fight_row)
-                db.session.commit()
+                add_row_to_battles()
                 return render_template('battle.html',
                                        main_pokemon=get_pokemon_data(session['main_pokemon']['name']),
                                        opponent_pokemon=get_pokemon_data(session['opponent_pokemon']['name']),
@@ -173,73 +154,25 @@ def battle(main_pokemon_name):
                                            auto_fight=False)
 
 
-@app.route("/battle-history")
+@app.route('/battle-history')
 def archive():
     battles = Battles.query.all()
     return render_template('battle_history.html',
                            battles=battles,
-                           some_list=['№', 'Покемон', 'Противник', 'Победитель'])
+                           some_list=['№', 'Покемон', 'Противник', 'Победитель', 'Пользователь'])
 
 
-@app.route("/email", methods=["POST"])
+@app.route('/email', methods=['POST'])
 def email():
-    sender = email_config['sender']
-    password = email_config['sender']
-    to_email = request.form.get('email_string')
     message = f'Ваш покемон: {session["main_pokemon"]["name"]}\n' \
               f'Противник: {session["opponent_pokemon"]["name"]}\n' \
               f'Победил: {winner()["name"]}'
-
-    msg = MIMEText(message)
-    msg['Subject'] = 'Результат боя'
-    msg['From'] = sender
-    msg['To'] = to_email
-
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(sender, password)
-    server.sendmail(sender, to_email, msg.as_string())
-
-    server.quit()
+    send_email.send_email(to_email=request.form.get('email_string'), theme='battle_result', content=message)
     return redirect('/')
 
 
-@app.route("/api/<pokemon_name>/save", methods=['POST'])
-def api_pokemon_save(pokemon_name):
-    return api.api_pokemon_save(pokemon_name)
-
-
-@app.route("/api/pokemon/list")
-def api_list():
-    return api.api_list()
-
-
-@app.route("/api/pokemon/<pokemon_id>")
-def api_pokemon(pokemon_id):
-    return api.api_pokemon(pokemon_id)
-
-
-@app.route("/api/pokemon/random")
-def api_random_pokemon():
-    return api.api_random_pokemon()
-
-
-@app.route("/api/fight")
-def api_fight():
-    return api.api_fight()
-
-
-@app.route("/api/fight/<number>", methods=['POST'])
-def api_fight_number(number):
-    return api.api_fight_number(number)
-
-
-@app.route("/api/fight/fast")
-def api_fast_fight():
-    return api.api_fast_fight()
-
-
 if __name__ == '__main__':
+    print(app.config['SQLALCHEMY_DATABASE_URI'])
     with app.app_context():
         db.create_all()
     app.run(host=os.environ.get('APP_HOST'), port=os.environ.get('APP_PORT'))
